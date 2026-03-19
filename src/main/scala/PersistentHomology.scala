63cf8670981935950870fbcd7b4df7bce16d967b
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2026-03-19 10:29:45 -0400

    First full computation!

--- src/main/scala/PersistentHomology.scala
+++ src/main/scala/PersistentHomology.scala
@@ -1,64 +1,81 @@
 package org.appliedtopology.tda4s
 
 import collection.mutable
-import scala.collection.immutable.SortedMap
+import scala.collection.immutable.{SortedMap, SortedSet}
+import scala.collection.{Set}
 
 object PersistentHomology:
-  def persistentHomology(simplexstream: Seq[Set[Int]], filtrationValues: Map[Set[Int], Double]): Seq[(Int, Double, Double)] =
+  def persistentHomology(simplexstream: Iterator[Set[Int]], filtrationValues: PartialFunction[Set[Int], Double]): Seq[(Int, Double, Double)] =
     NaivePersistentHomology(simplexstream, filtrationValues).barcode
 end PersistentHomology
 
-class NaivePersistentHomology(simplexstream: Seq[Set[Int]], filtrationValues: Map[Set[Int], Double]):
+class NaivePersistentHomology(simplexstream: Iterator[Set[Int]], filtrationValues: PartialFunction[Set[Int], Double]):
+  def filtrationValue(simplex : Set[Int]) : Double =
+    filtrationValues.applyOrElse(simplex, (_) => Double.PositiveInfinity)
+  def streamOrdering = Ordering.by(filtrationValue).orElseBy(SortedSet.from(_))(Ordering.Implicits.sortedSetOrdering)
   case class State(
       cycleBasis: Map[Set[Int], Chain],
       boundaryBasis: Map[Set[Int], Chain],
-      coboundaryLookup: Map[Set[Int], Set[Int]],
+      coboundaryLookup: Map[Set[Int], Chain],
       cycleBirths: Map[Set[Int], Double]
   )
   var state = State(Map.empty, Map.empty, Map.empty, Map.empty)
   val barcodes = mutable.ListBuffer.empty[(Int, Double, Double)]
   def step(simplex: Set[Int]): Unit = if (simplex.size <= 1) {
-    val birth = filtrationValues.getOrElse(simplex, Double.PositiveInfinity)
+    val birth = filtrationValue(simplex)
     state = state.copy(
       cycleBasis = state.cycleBasis + (simplex -> Chain(
-        SortedMap(simplex -> 1.0)(using Ordering.by(filtrationValues.getOrElse(_, Double.PositiveInfinity)))
+        SortedMap(simplex -> 1.0)(using streamOrdering)
       )),
       cycleBirths = state.cycleBirths + (simplex -> birth)
     )
   } else {
-    val dsimplex = Chain.boundary(simplex)(Ordering.by(filtrationValues.getOrElse(_, Double.PositiveInfinity)))
+    val dsimplex = Chain.boundary(simplex)(streamOrdering)
     val dimension = simplex.size - 1
+    assert(dsimplex.entries.size == simplex.size)
     val (dsModB, reductionB) = Chain.reduceBy(dsimplex, state.boundaryBasis)
     if (dsModB.isZero) {
       // then ∂s is already a boundary, say the boundary of z
       // hence ∂(s-z) = ∂s - ∂z = 0
       // so s-z is a new cycle
-      val cycle = Chain(
-        SortedMap.from(reductionB.map((s, v) => (state.coboundaryLookup(s), v)))(using
-          Ordering.by(filtrationValues.getOrElse(_, Double.PositiveInfinity))
-        ) + (simplex -> 1.0)
-      )
-      val (cyclemodZ, _) = Chain.reduceBy(cycle, state.cycleBasis)
-      val cycleBirth = filtrationValues.getOrElse(simplex, Double.PositiveInfinity)
+      val cycle = // look through reduction log, read off coboundaries and scale and add
+        reductionB
+          .foldLeft(Chain.from(simplex)(streamOrdering)) { case (chain, (spx, coeff)) =>
+            chain.scaleAdd(-coeff, state.coboundaryLookup(spx))
+          }
 
-      state = cyclemodZ.entries.headOption match {
-        case Some((spx, _)) =>
-          state.copy(cycleBasis = state.cycleBasis + (spx -> cyclemodZ), cycleBirths = state.cycleBirths + (spx -> cycleBirth))
+      val (cyclemodZ, _) = Chain.reduceBy(cycle, state.cycleBasis)
+      val cycleBirth = filtrationValue(simplex)
+
+      state = cyclemodZ.leading match {
+        case Some((spx, _)) => {
+          state.copy(
+            cycleBasis = state.cycleBasis + (spx -> cyclemodZ),
+            cycleBirths = state.cycleBirths + (spx -> cycleBirth),
+          )
+        }
         case None: Option[(Set[Int], Double)] => state
       }
     } else {
       // since ∂s is not a boundary already, it has to be a cycle
       // reducing mod cycles will tell us which cycle just became a boundary with the addition of simplex
-      val (dsModZ, reductionZ) = Chain.reduceBy(dsModB, state.cycleBasis)
-      assert(dsModZ.isZero)
-      val dyingChainHead = reductionZ.keys
-        .maxBy(filtrationValues.getOrElse(_, Double.PositiveInfinity))
-      barcodes.append((dyingChainHead.size - 1, state.cycleBirths(dyingChainHead), filtrationValues(simplex)))
+      //val (dsModZ, reductionZ) = Chain.reduceBy(dsModB, state.cycleBasis)
+      //assert(dsModZ.isZero)
+      val dyingChainHead = dsModB
+        .entries
+        .keys
+        .filter((k) => state.cycleBasis.contains(k))
+        .max(using streamOrdering)
+      barcodes.append((dyingChainHead.size - 1, state.cycleBirths(dyingChainHead), filtrationValue(simplex)))
+      assert(simplex.size == dyingChainHead.size + 1)
       state = state.copy(
         cycleBasis = state.cycleBasis - dyingChainHead,
-        boundaryBasis = state.boundaryBasis + (dyingChainHead -> state.cycleBasis(dyingChainHead)),
+        boundaryBasis = state.boundaryBasis + (dsModB.leading.get._1 -> dsModB),
         cycleBirths = state.cycleBirths - dyingChainHead,
-        coboundaryLookup = state.coboundaryLookup + (dyingChainHead -> simplex)
+        coboundaryLookup = state.coboundaryLookup.updatedWith(dyingChainHead) {
+          case Some(value) => Some(value.scaleAdd(1.0, Chain.from(simplex)(streamOrdering)))
+          case None => Some(Chain.from(simplex)(streamOrdering))
+        }
       )
     }
   }
