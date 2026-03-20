2fcb5a5a8f07e7bb00d66669b8cb6155df5b20d4
Mikael Vejdemo-Johansson <michiexile@gmail.com>
2026-03-20 11:17:05 -0400

    More idiomatic code style.

--- src/main/scala/VietorisRips.scala
+++ src/main/scala/VietorisRips.scala
@@ -4,15 +4,13 @@ import scala.collection.Set
 
 trait VietorisRips(val metricSpace: MetricSpace[Int]):
   def filtrationValues: PartialFunction[Set[Int], Double] = {
-    case spx if spx.size == 0 => Double.NegativeInfinity
+    case spx if spx.isEmpty   => Double.NegativeInfinity
     case spx if spx.size == 1 => 0.0
     case spx =>
-      spx.toSeq.combinations(2)
-        .map { case Seq(x, y) => metricSpace.distance(x, y) }
-        .max
+      spx.toSeq.combinations(2).map { case Seq(x, y) => metricSpace.distance(x, y) }.max
   }
   def simplicesInDimension(dimension: Int): Iterator[Set[Int]]
-  def simplices(): Iterator[Set[Int]] = (0 until metricSpace.elements.size).iterator.flatMap(simplicesInDimension)
+  def simplices(): Iterator[Set[Int]] = metricSpace.elements.indices.iterator.flatMap(simplicesInDimension)
 
 class NaiveVietorisRips(metricSpace: MetricSpace[Int]) extends VietorisRips(metricSpace):
   override def simplicesInDimension(dimension: Int): Iterator[Set[Int]] =
